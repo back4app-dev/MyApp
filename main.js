c6d8945b0029debceee719b9ddd8a5c97e4e627d
Example <example@example.com>
2023-11-14 18:59:47 +0000

    Deploying main.js file from Back4apps Cloud Code

---
+++ main.js
@@ -0,0 +1,33 @@
+Parse.Cloud.beforeLogin(async (request) => {
+  const user = request.object;
+
+  if (user.get("disabled")) {
+    throw new Error(
+      "Your account has been disabled. Please contact an Administrator."
+    );
+  }
+
+  if (user.get("deleted")) {
+    throw new Error(
+      "Your account was permanently deleted. You can not log in."
+    );
+  }
+});
+
+Parse.Cloud.beforeSave(Parse.User, async (request) => {
+  const user = request.object;
+
+  if (!user.existed()) {
+    const requiredProperties = ["username", "email"];
+    const missingProperties = requiredProperties.filter(
+      (property) => !user.has(property)
+    );
+
+    if (missingProperties.length > 0) {
+      throw new Parse.Error(
+        Parse.Error.VALIDATION_ERROR,
+        `The following properties are required: ${missingProperties.join(", ")}`
+      );
+    }
+  }
+});
